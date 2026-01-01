ca5166d7b5ded1aca97774b9734c85b641985b62
lindoad <lindoad22@sps-prosek.cz>
2026-01-02 00:30:02 +0100

    lib_import wifi

--- code.py
+++ code.py
@@ -1,12 +1,28 @@
 import time
 import usb_hid
+import wifi
+import socketpool
 
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from adafruit_hid.keycode import Keycode
 
+from adafruit_httpserver import Request, Response, Server
 
+
+print("creating AP")
 time.sleep(3)
+wifi.radio.start_ap(ssid="test", password="12345678")
+
+pool = socketpool.SocketPool(wifi.radio)
+server = Server(pool, "/static", debug=True)
+
+@server.route("/static")
+
+def base(request: Request):
+    return Response(request, "Hello from python server")
+
+server.serve_forever(str(wifi.radio.ipv4_address_ap))
 
 def run_payload():
 
@@ -26,6 +42,8 @@ def run_payload():
 
     #kbd.press(Keycode.ENTER)
     kbd.release_all()
+    
+print("hello")
 
 
 #run_payload()
