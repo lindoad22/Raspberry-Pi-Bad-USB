0591e60b59a604a2d5fa2c78f0d4a5ff42782578
lindoad <lindoad22@sps-prosek.cz>
2026-01-03 21:50:02 +0100

    ducky_script

--- code.py
+++ code.py
@@ -2,62 +2,118 @@ import time
 import usb_hid
 import wifi
 import socketpool
-import microcontoller
+import json
+import ducky_script
+from fce import load_json
 
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from adafruit_hid.keycode import Keycode
 
-from adafruit_httpserver import Request, Response, Server
+from adafruit_httpserver import GET,POST, Request, Response, Server
 
 def attack_mode():
+    #load ducky_script keymap
+    dictionary = ducky_script.get_dict()
+    #print(dictionary)
+    
+    #
+    active_id = load_json("/data/active_script.json")["active_script_id"]
+    active_script = load_json("/data/scripts.json")[active_id]
+
+    split_script = active_script["script"].split(",")
+
+    kbd = Keyboard(usb_hid.devices)
+    layout = KeyboardLayoutUS(kbd)
+
+    for i in split_script:
+        if "+" in i:
+            i_split = i.split("+")
+            #print(i_split)
+            
+            for x in i_split:
+                key = dictionary[x]
+                #print(key)
+                
+                kbd.press(key)
+                print("pressing")
+
+
+            time.sleep(0.2)
+            kbd.release_all()
+            print("keys released")
+
+        elif "[" and "]" in i:
+            layout.write(i[1:-1])
+            print("writing")
+
+        else:
+            key = dictionary[i]
+            kbd.send(key)
+            
+
+
+            
+
+    
+
+def config_mode():
+    def open_html(path):
+        with open(path) as f:
+            return f.read()
+    
+    with open("/data/network.json") as f:
+        network_file = json.load(f)
+
+    print(network_file)
+    
+    ssid = network_file["ssid"]
+    wifi_password = network_file["wifi_password"]
+    script = ""
+
+
     print("creating AP")
-    hello("text")
-    time.sleep(3)
-    wifi.radio.start_ap(ssid="test", password="12345678")
+    #time.sleep(3)
+    wifi.radio.start_ap(ssid=ssid, password=wifi_password)
 
     pool = socketpool.SocketPool(wifi.radio)
     server = Server(pool, "/static", debug=True)
+    
 
-    @server.route("/static")
+    #file = [('ssid', 'test'), ('ip_address', '192'), ('wifi_password', '12345678'), ('script', 'Script')]
+    #print(file[0])
 
-    def base(request: Request):
-        return Response(request, "Hello from python server")
+    
+    @server.route("/", methods=[GET, POST])
+    def index(request: Request):
+
+        if request.method == "POST":
+            posted_value = request.form_data
+
+            data = {key: value for key, value in posted_value.items()}
+            print(data)
+
+            with open("/data/network.json", 'w') as f:
+                json.dump(data, f)
+        elif request.method == "GET":
+            print("NOT POST")
+        
+        html_page = open_html("/static/settings.html")
+
+        return Response(request, html_page.format(
+            ssid_value=(ssid),
+            wifi_password_value=(wifi_password),
+            script=(script)
+
+        ), content_type="text/html")
+    
+    
+    
 
     server.serve_forever(str(wifi.radio.ipv4_address_ap))
-
-    def run_payload():
-
-        kbd = Keyboard(usb_hid.devices)
-        layout = KeyboardLayoutUS(kbd)
-
-        kbd.press(Keycode.WINDOWS, Keycode.R)
-        time.sleep(0.2)
-        kbd.release_all()
-
-        layout.write("cmd")
-        kbd.press(Keycode.ENTER)
-        time.sleep(0.5)
-
-        layout.write("nc -lvnp 444") #script
-        time.sleep(0.3)
-
-        #kbd.press(Keycode.ENTER)
-        kbd.release_all()
-        
-    print("hello")
-
-def config_mode():
-    pass
-
-if microcontoller.boot_button:
-    config_mode()
-
-else:
-    attack_mode()
-
-
-#run_payload()
+    
+attack_mode()
+#config_mode()
 
 while True:
     time.sleep(1)
