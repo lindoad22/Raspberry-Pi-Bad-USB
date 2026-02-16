78118122f22a1363d54c908606e3215e0ae204e8
Adam Lindourek <lindoad22@sps-prosek.cz>
2026-02-16 19:14:49 +0100

    Logging to files done

--- code.py
+++ code.py
@@ -20,15 +20,6 @@ import board
 import storage  
 
 
-# button = digitalio.DigitalInOut(board.GP15)
-# button.switch_to_input(pull=digitalio.Pull.UP)
-# while True:
-#     if button.value == False:
-#         print("pushed")
-#     else:
-#         print("not pushed")
-
-#     time.sleep(0.2)
 
 
 sck = board.GP18
@@ -41,7 +32,7 @@ spi = busio.SPI(sck, mosi, miso)
 #mounting SD card
 sdcard = adafruit_sdcard.SDCard(spi, cs)
 vfs = storage.VfsFat(sdcard)
-storage.mount(vfs, "/sd")
+storage.mount(vfs, '/sd')
 
 def attack_mode():
     #load ducky_script keymap
@@ -49,48 +40,69 @@ def attack_mode():
     #print(dictionary)
     
     #loading json file with active script ID
-    active_id = load_json("/sd/active_script.json")["active_script_id"]
+    active_id = load_json('/sd/active_script.json')['active_script_id']
     #loading active attack script from json file
-    active_script = load_json("/sd/scripts.json")[active_id]
-    split_script = active_script["script"].split(",")
-
+    active_script = load_json('/sd/scripts.json')[active_id]
+    split_script = active_script['script'].split(',')
     #set keyboard
     kbd = Keyboard(usb_hid.devices)
     layout = KeyboardLayoutUS(kbd)
 
-
+    error = ''
+    status = ''
     #Generating HID inputs from ducky script
-    for i in split_script:
-        if "+" in i:
-            i_split = i.split("+")
-            #print(i_split)
-            
-            for x in i_split:
-                key = dictionary[x]
-                #print(key)
+    try:
+
+        for i in split_script:
+            if '+' in i:
+                i_split = i.split('+')
+                #print(i_split)
                 
-                kbd.press(key)
-                print("pressing")
+                for x in i_split:
+                    key = dictionary[x]
+                    #print(key)
+                    
+                    kbd.press(key)
+                    print('pressing')
 
 
-            time.sleep(0.2)
-            kbd.release_all()
-            print("keys released")
+                time.sleep(0.2)
+                kbd.release_all()
+                print('keys released')
 
-        elif "[" and "]" in i:
-            layout.write(i[1:-1])
-            print("writing")
+            elif '[' and ']' in i:
+                layout.write(i[1:-1])
+                print('writing')
 
-        elif "WAIT" in i:
-            t = i[4:]
-            time.sleep(float(t))
+            elif 'WAIT' in i:
+                t = i[4:]
+                time.sleep(float(t))
 
-        else:
-            key = dictionary[i]
-            kbd.send(key)
+            else:
+                key = dictionary[i]
+                kbd.send(key)
 
+            status = 'success'
+
+    except:
+
+        error = 'syntax error in script'
+        status = 'failed'
+
+
+    logs_file = load_json('/sd/logs.json')
+
+    print(active_script['name'])
+    log = {
+        'status': status,
+        'reason': error,
+        'script_name': active_script['name']
+        }
     
-    #log attack to file
+    logs_file['logs'].append(log)
+    #logs_file = {'logs': [{'reason': '', 'status': 'success', 'script_name': 'test script'}]}
+    write_json_file('/sd/logs.json', logs_file)
+
 
 def config_mode():
 
@@ -100,41 +112,41 @@ def config_mode():
             return f.read()
     
     #laoding data from files
-    network_file = load_json("/sd/network.json")
-    users = load_json("/sd/login.json")
+    network_file = load_json('/sd/network.json')
+    users = load_json('/sd/login.json')
 
-    ssid = network_file["ssid"]
-    wifi_password = network_file["wifi_password"]
+    ssid = network_file['ssid']
+    wifi_password = network_file['wifi_password']
 
-    print("creating AP")
+    print('creating AP')
     #time.sleep(3)
     wifi.radio.start_ap(ssid=ssid, password=wifi_password)
 
     pool = socketpool.SocketPool(wifi.radio)
-    server = Server(pool, "/static", debug=True)
+    server = Server(pool, '/static', debug=True)
 
     #page settings on root directory
-    @server.route("/", methods=[GET, POST])
+    @server.route('/', methods=[GET, POST])
     def index(request: Request):
         
         ssid = load_json('/sd/network.json')
-        ssid = network_file["ssid"]
-        wifi_password = network_file["wifi_password"]
-        login_username = load_json('/sd/login.json')["username"]
+        ssid = network_file['ssid']
+        wifi_password = network_file['wifi_password']
+        login_username = load_json('/sd/login.json')['username']
 
         #request cookies
-        session_ID = request.headers.get("Cookie", "")
+        session_ID = request.headers.get('Cookie', '')
         #print(session_ID)
 
-        #session_ID = {"session_ID":"1"}
+        #session_ID = {'session_ID':'1'}
 
-        message = ""
+        message = ''
         if False == bool(session_ID):
-            return Redirect(request, "/login")
+            return Redirect(request, '/login')
             pass
         else:
             
-            if request.method == "POST":
+            if request.method == 'POST':
                 posted_value = request.form_data
 
                 #processing form data 
@@ -165,17 +177,17 @@ def config_mode():
                     write_json_file('/sd/login.json', login_file)
 
                     #update forms input placeholders
-                    message="Settings saved"
+                    message='Settings saved'
                     ssid = load_json('/sd/network.json')
-                    ssid = network_file["ssid"]
-                    wifi_password = network_file["wifi_password"]
-                    login_username = load_json('/sd/login.json')["username"]
+                    ssid = network_file['ssid']
+                    wifi_password = network_file['wifi_password']
+                    login_username = load_json('/sd/login.json')['username']
 
-            elif request.method == "GET":
-                #print("NOT POST")
+            elif request.method == 'GET':
+                #print('NOT POST')
                 pass
             
-            html_page = open_html("/static/settings.html")
+            html_page = open_html('/static/settings.html')
 
             return Response(request, html_page.format(
                 ssid_value=(ssid),
@@ -183,82 +195,86 @@ def config_mode():
                 login_username=(login_username),
                 message=(message)
 
-            ), content_type="text/html")
+            ), content_type='text/html')
     
     #page settings on /login directory
-    @server.route("/login", methods=[GET, POST])
+    @server.route('/login', methods=[GET, POST])
     def start(request: Request):
-        session_ID = request.headers.get("Cookie", "")
+        session_ID = request.headers.get('Cookie', '')
 
         print(session_ID)
         if False == bool(session_ID):
             
-            message = ""
-            if request.method == "POST":
+            message = ''
+            if request.method == 'POST':
                 posted_value = request.form_data
                 data = {key: value for key, value in posted_value.items()}
 
-                login_file = load_json("/sd/login.json")
-                if data["username"] == login_file["username"]:
+                login_file = load_json('/sd/login.json')
+                if data['username'] == login_file['username']:
                     if data['password'] == login_file['password']:
-                        #cookies = {"session_ID": "1"}
+                        #cookies = {'session_ID': '1'}
 
                         headers = Headers()
-                        headers.add("Set-Cookie", "session_ID=1; Path=/")
-                        print("headers set")
-                        return Redirect(request, "/",headers=headers)
+                        headers.add('Set-Cookie', 'session_ID=1; Path=/')
+                        print('headers set')
+                        return Redirect(request, '/',headers=headers)
                     else:
-                        message = "wrong username or password"
+                        message = 'wrong username or password'
                         print(message)
 
                 else:
-                        message = "wrong username or password"
+                        message = 'wrong username or password'
                         print(message)
 
                 
             
-            html_page = open_html("/static/login.html")
-            return Response(request, html_page.format(message=message), content_type="text/html")
+            html_page = open_html('/static/login.html')
+            return Response(request, html_page.format(message=message), content_type='text/html')
         
         else:
-            return Redirect(request, "/")
+            return Redirect(request, '/')
 
-    @server.route("/script_edit", methods=[GET, POST])
+    @server.route('/script_edit', methods=[GET, POST])
     def edit(request: Request):
         #session_ID = request.cookies
 
-        list_output = ""
-        script_file = load_json("/sd/scripts.json")
-        active_script = load_json("/sd/active_script.json")
+        list_output = ''
+        script_file = load_json('/sd/scripts.json')
+        active_script = load_json('/sd/active_script.json')
         active_script_id = active_script['active_script_id']
-        script = ""
-        sc_id = ""
-        message =""
+        script = ''
+        sc_id = ''
+        message =''
+        script_name = ''
 
-        list_item = open_html("/static/list_item.html")
+        list_item = open_html('/static/list_item.html')
         for key,value in script_file.items():
 
-            l_item = list_item.format(script_id=key, script_name=value["name"], content_type="text/html")
+            l_item = list_item.format(script_id=key, script_name=value['name'], content_type='text/html')
             list_output = list_output + l_item
 
-        checked = ""
-        if request.method == "GET":
+        checked = ''
+        if request.method == 'GET':
 
             get = request.query_params
 
             if len(get) != 0:
 
-                sc_id = get["sc_id"]
+                sc_id = get['sc_id']
                 print(sc_id)
                 print(active_script_id)
 
                 try:
 
-                    script = script_file[sc_id]["script"]
+                    script = script_file[sc_id]['script']
+                    script_name = script_file[sc_id]['name']
+                    if sc_id == active_script_id:
+                        checked = 'checked'
                 except:
                     pass
 
-        if request.method == "POST":
+        if request.method == 'POST':
             posted_value = request.form_data
             get = request.query_params
             
@@ -266,40 +282,111 @@ def config_mode():
 
                 data = {key: value for key, value in posted_value.items()}
                 print(data)
-                #print(get["sc_id"])
+                #print(get['sc_id'])
 
-                script_file[get['sc_id']]["script"] = data["script"]
-                print(script_file)
-                message = "Script saved"
+                if 'delete_script' in data:
+                    if data['delete_script'] == 'on':
 
-        html_page = open_html("/static/script_edit.html")
-        return Response(request, html_page.format(checked=checked,script=script, list_item=list_output, message=message), content_type="text/html")
+                        script_file = load_json('/sd/scripts.json')
+
+                        delete_id = str(data['script_id'])
+
+                        if delete_id in script_file:
+                            del script_file[delete_id]
+
+                            write_json_file('/sd/scripts.json', script_file)
+                        else:
+                            print("Script not found")
+
+                        message = 'Script deleted'
+
+                elif 'active_script' in data:
+                    if data['active_script'] == 'on':
+                        active_script_id = str(data['script_id'])
+                        active_script['active_script_id'] = active_script_id
+                        #print(active_script)
+
+                        write_json_file('/sd/active_script.json', active_script)
+                        message = 'Script saved'
+
+                elif 'new_script' in data:
+                    if data['new_script'] == 'on':
+                        script_file = load_json('/sd/scripts.json')
+                        ids = [int(k) for k in script_file.keys()]
+                        new_id = str(max(ids) + 1)
+
+                        #print(type(new_id))
+                        script_file[new_id] = {'name': data['name'],
+                                               'script': data['script']
+                                               }
+
+                        #print(script_file)
+                        write_json_file('/sd/scripts.json', script_file)
+
+                        message = 'Script saved'
+
+                        
+                script_file = load_json('/sd/scripts.json')
+                active_script = load_json('/sd/active_script.json')
+                active_script_id = active_script['active_script_id']
+
+                list_output = ''
+                list_item = open_html('/static/list_item.html')
+                for key,value in script_file.items():
+
+                    l_item = list_item.format(script_id=key, script_name=value['name'], content_type='text/html')
+                    list_output = list_output + l_item
+                #script_file[get['sc_id']]['script'] = data['script']
+                #print(script_file)
+
+        html_page = open_html('/static/script_edit.html')
+        return Response(request, html_page.format(
+            check=checked,
+            script=script,
+            list_item=list_output,
+            message=message,
+            script_name = script_name,
+            sc_id = sc_id
+            ), content_type='text/html')
     
-    @server.route("/logbook", methods=[GET, POST])
+    @server.route('/logbook', methods=[GET, POST])
     def edit(request: Request):
 
-        html_page = open_html("/static/logbook.html")
+        html_page = open_html('/static/logbook.html')
         
-        logs = []
-        with open("/data/logs.ndjson") as f:
-            for line in f:
-                logs.append(json.loads(line))
+        log_file = load_json('sd/logs.json')['logs']
 
-        list_item = open_html("/static/log_item.html")
-        list_output = ""
-        for i in logs:
+        list_item = open_html('/static/log_item.html')
+        list_output = ''
+        
+        id_counter = 0
+        for i in log_file:
             print(i)
+
             
-            l_item = list_item.format(id=i["id"], status=i["status"], reason=i["reason"],script_name=i["script_name"], content_type="text/html")
+            l_item = list_item.format(id=id_counter, status=i['status'], reason=i['reason'],script_name=i['script_name'], content_type='text/html')
             list_output = list_output + l_item
+
+            id_counter += 1
         
         
-        return Response(request, html_page.format(logbook_item=list_output), content_type="text/html")
+        return Response(request, html_page.format(logbook_item=list_output), content_type='text/html')
 
     server.serve_forever(str(wifi.radio.ipv4_address_ap))
-    
+
+
 #attack_mode()
 config_mode()
+
+
+# attack = 0
+# config = 1
+
+# button = digitalio.DigitalInOut(board.GP15)
+# button.switch_to_input(pull=digitalio.Pull.UP)
+
+# current_mode = attack
+# last_button_state = button.value
 
 while True:
     time.sleep(1)
